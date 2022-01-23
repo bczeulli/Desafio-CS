3daddea2f43d2ce5809885e0e11a1b8fa1d70ebe
Bczeulli <bczeulli@gmail.com>
2022-01-23 10:11:08 -0300

    melhorando a function editar
    uma unica function com um switch

--- scripts.js
+++ scripts.js
@@ -1,32 +1,29 @@
 
 // botão editar habilita o form
-function editar1() {
-    var input = document.getElementById('textArea1').readOnly = false;
+
+function editar(n) {
+    switch (n) {
+        case 1:
+            var input = document.getElementById('textArea1').readOnly = false;
+            break;
+        case 2:
+            var input = document.getElementById('textArea2').readOnly = false;
+            break;
+        case 3:
+            var input = document.getElementById('textArea3').readOnly = false;
+            break;
+        case 4:
+            var input = document.getElementById('textArea4').readOnly = false;
+            break;
+        case 5:
+            var input = document.getElementById('textArea5').readOnly = false;
+            break;
+        case 6:
+            var input = document.getElementById('textArea6').readOnly = false;
+            break;
+        case 7:
+            var input = document.getElementById('textArea7').readOnly = false;
+            break;                     
+    }
 }
-
-function editar2() {
-    var input = document.getElementById('textArea2').readOnly = false;
-}
-
-function editar3() {
-    var input = document.getElementById('textArea3').readOnly = false;
-}
-
-function editar4() {
-    var input = document.getElementById('textArea4').readOnly = false;
-}
-
-function editar5() {
-    var input = document.getElementById('textArea5').readOnly = false;
-}
-
-function editar6() {
-    var input = document.getElementById('textArea6').readOnly = false;
-}
-
-function editar7() {
-    var input = document.getElementById('textArea7').readOnly = false;
-}
-
-
 
