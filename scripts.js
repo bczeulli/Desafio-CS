c02b8ddd70c2146b412e27aa5c922aa88810e765
Bczeulli <bczeulli@gmail.com>
2022-01-26 14:30:24 -0300

    novas alterações nos botões

--- scripts.js
+++ scripts.js
@@ -5,25 +5,45 @@ function editar(n) {
     switch (n) {
         case 1:
             var input = document.getElementById('textArea1').readOnly = false;
-            break;
+            
         case 2:
             var input = document.getElementById('textArea2').readOnly = false;
-            break;
+            
         case 3:
             var input = document.getElementById('textArea3').readOnly = false;
-            break;
+            
         case 4:
             var input = document.getElementById('textArea4').readOnly = false;
-            break;
+            
         case 5:
             var input = document.getElementById('textArea5').readOnly = false;
-            break;
+            
         case 6:
             var input = document.getElementById('textArea6').readOnly = false;
-            break;
+            
         case 7:
             var input = document.getElementById('textArea7').readOnly = false;
             break;                     
     }
 }
+
+/* function editar(n1,n2,n3,n4,n5,n6,n7){
+    if (n1 == 1) {
+        let input1 = document.getElementById('textArea1').readOnly = false;
+    } else if (n2 == 2) {
+        let input2 = document.getElementById('textArea2').readOnly = false;
+    } else if (n3 == 3) {
+        let input3 = document.getElementById('textArea3').readOnly = false;
+    } else if (n4 == 4) {
+        let input4 = document.getElementById('textArea4').readOnly = false;  
+    } else if (n5 == 5) {
+        let input4 = document.getElementById('textArea5').readOnly = false;  
+    } else if (n6 == 6) {
+        let input4 = document.getElementById('textArea6').readOnly = false;  
+    } else if (n7 == 7) {
+        let input4 = document.getElementById('textArea7').readOnly = false;  
+    } 
+    
+} */
+
 
